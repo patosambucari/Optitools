59a1c299d3eaea6c1bf7db9c0073bb0e6e98f568
patosambucari <patosambucari@gmail.com>
2022-01-27 19:54:10 -0300

    Rearmando Distometria con DOM - 20220127 19:54

--- V1/tools/js/borrador.js
+++ V1/tools/js/borrador.js
@@ -1,26 +1,29 @@
-//Lleno el array con las graduaciones y lentes disponibles
-for (let esf = -20.00; esf <= -12.50; esf += 0.25){
-    graduacionesEsfericasDisponibles.push(new GraduacionEsf(esf, -0.00, 0, "Proclear, Biofinity XR"));
-    }
-    for (let esf = -12.00; esf <= -0.250; esf += 0.25){
-        graduacionesEsfericasDisponibles.push(new GraduacionEsf(esf, -0.00, 0, "Air Optix, Avaira, Biofinity"));
-    }
-    for (let esf = +0.25; esf <= +8.00; esf += 0.25){
-            graduacionesEsfericasDisponibles.push(new GraduacionEsf(esf, -0.00, 0, "Air Optix, Avaira, Biofinity"));
-    }
-    for (let esf = +8.25; esf <= +15.00; esf += 0.25){
-                graduacionesEsfericasDisponibles.push(new GraduacionEsf(esf, -0.00, 0, "Proclear, Biofinity XR"));
-    }
-    for (let esf = -9.00; esf <= +6.00; esf +=0.25){
-        graduacionesEsfericasDisponibles.marcasDisponibles = (`${graduacionesEsfericasDisponibles.marcasDisponibles}, Soflens59, Biomedics`);
-    }    
+var botonOD = document.getElementById("botonOD");
+var botonOI = document.getElementById("botonOI");
+var botonAO = document.getElementById("botonAO");
+var datosOD = document.getElementById("ingresoDatosOD");
+var datosOI = document.getElementById("ingresoDatosOI");
 
+console.log(botonOD);
+console.log(botonOI);
+console.log(botonAO);
+console.log(datosOD);
+console.log(datosOI);
 
+botonOD.addEventListener("click", (event) => {
+  event.preventDefault();
+  datosOD.style.display = "block";
+  datosOI.style.display = "none";
+});
 
+botonOI.addEventListener("click", (event) => {
+  event.preventDefault();
+  datosOD.style.display = "none";
+  datosOI.style.display = "block";
+});
 
-
-    /*if (recetaAdaptada.esfOjo2 !== null && Math.abs(recetaAdaptada.cilOjo2) <= 1.75){
-        recetaEsfSugerida.esfOjo2 = calcularEsfSugerido(recetaAdaptada.esfOjo2, recetaAdaptada.cilOjo2);
-    } else {
-        recetaEsfSugerida.esfOjo2 = null;
-    }*/
+botonAO.addEventListener("click", (event) => {
+  event.preventDefault();
+  datosOD.style.display = "block";
+  datosOI.style.display = "block";
+});
